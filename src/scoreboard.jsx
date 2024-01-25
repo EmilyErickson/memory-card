bf65379ecc967209dcbe46b95e22e0f68bdd45c0
EmilyErickson <emilyerickson2002@gmail.com>
2024-01-25 11:09:18 -0600

    Create initial scoreboard div

---
+++ src/scoreboard.jsx
@@ -0,0 +1,50 @@
+
+
+export default function Scoreboard() {
+    return (
+    <div>
+        <h4>Scoreboard</h4>
+    </div>
+    )
+}
+
+
+
+
+    // const [cardArray, setCardArray] = useState([
+    //     {key: uuidv4(), cardNumber: 1, clicked: false},
+    //     {key: uuidv4(), cardNumber: 2, clicked: false},
+    //     {key: uuidv4(), cardNumber: 3, clicked: false},
+    //     {key: uuidv4(), cardNumber: 4, clicked: false},
+    //     {key: uuidv4(), cardNumber: 5, clicked: false},
+    //     {key: uuidv4(), cardNumber: 6, clicked: false},
+    //     {key: uuidv4(), cardNumber: 7, clicked: false},
+    //     {key: uuidv4(), cardNumber: 8, clicked: false},
+    //     {key: uuidv4(), cardNumber: 9, clicked: false},
+    //     {key: uuidv4(), cardNumber: 10, clicked: false},
+    //     {key: uuidv4(), cardNumber: 11, clicked: false},
+    //     {key: uuidv4(), cardNumber: 12, clicked: false},
+    //     {key: uuidv4(), cardNumber: 13, clicked: false},
+    //     {key: uuidv4(), cardNumber: 14, clicked: false},
+    //     {key: uuidv4(), cardNumber: 15, clicked: false},
+    // ])
+
+
+    // function handleClick(card) {
+    //     console.log(card)
+    //     if(card.clicked === true) {
+    //         alert("clicked")
+    //     }
+    //     card.clicked = true
+    //     shuffleCards()
+    // }
+
+    // function shuffleCards() {
+    //     const shuffledCards = [...cardArray];
+    //     for (let i = shuffledCards.length - 1; i > 0; i--) {
+    //       const j = Math.floor(Math.random() * (i + 1));
+    //       [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+    //     }
+    //     setCardArray(shuffledCards);
+    //   }
+    
